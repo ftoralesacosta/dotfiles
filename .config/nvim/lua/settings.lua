6b221b2fa991a6e5dfd152d58c00ff3cc30ad862
Fernando <fernando@MacBook-Air.local>
2022-03-08 15:31:32 -0800

    added relative numbers

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -4,5 +4,7 @@ vim.opt.syntax = "enable"
 vim.opt.inccommand = 'nosplit' --replace live preview %s
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.wo.number = true
+vim.wo.relativenumber = true
 
 vim.api.nvim_exec([[ autocmd BufEnter * set fo-=o fo-=c fo-=r ]],false)
