606bc50cb884085d124db22b1334aec94ad813b4
Fernando <fernando@fc-e2-6c-e-d1-b9.dhcp.lbnl.us>
2022-05-16 15:54:10 -0700

    moved nvim out of .config

---
+++ nvim/init.lua
@@ -0,0 +1,23 @@
+---------------
+-- Lua Files --
+---------------
+require('options')
+require('plugins')
+require('maps')
+require('colorscheme')
+require('completion')
+require('whichkey')
+require('tele_scope')
+require('nvim_tree')
+
+----------------
+-- TreeSitter --
+----------------
+local configs = require'nvim-treesitter.configs'
+configs.setup
+	{	--"maintained" list does not fully compile
+		ensure_installed = "bash","cpp","python","lua",
+				"latex","yaml","cmake","c","c_sharp",
+		highlight = { enable = true,}, --enable highlighting
+		indent = { enable = true,} --default is disabled anyways
+	}
---
+++ nvim/lua/colorscheme.lua
@@ -0,0 +1,10 @@
+-- vim.opt.termguicolors = true
+
+-- Background --
+-- vim.o.background = "dark" -- or "light" 
+
+-- SELECT -- 
+vim.cmd[[colorscheme gruvbox]]
+-- vim.cmd[[colorscheme everforest]]
+-- vim.cmd[[colorscheme onedarker]]
+-- vim.cmd[[colorscheme tokyonight]]
